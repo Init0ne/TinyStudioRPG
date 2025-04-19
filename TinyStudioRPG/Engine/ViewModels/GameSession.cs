b1fa9b9584a5dfce6d8da1a27f544bdc0f0bdcf5
Mathias Falvo <falvomathias@gmail.com>
2025-04-19 11:34:14 -0300

    * Creo proyecto Engine * * Creo Models y ViewModels * * * Creo Player y GameSession clases

---
+++ TinyStudioRPG/Engine/ViewModels/GameSession.cs
@@ -0,0 +1,18 @@
+﻿using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        Player CurrentPlater { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlater = new Player
+            {
+                Name = "Martín Fierro",
+                Gold = 100,
+            };
+        }
+    }
+}
