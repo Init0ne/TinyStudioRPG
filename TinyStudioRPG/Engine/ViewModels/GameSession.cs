330fcea2152f4aa35d6d136166ad7bff50beaa27
Mathias Falvo <falvomathias@gmail.com>
2025-04-21 00:21:18 -0300

    * Creo la primer locacion * Agrego carpeta para imagenes * Agrega primer imagen de locacion

--- TinyStudioRPG/Engine/ViewModels/GameSession.cs
+++ TinyStudioRPG/Engine/ViewModels/GameSession.cs
@@ -5,6 +5,7 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
 
         public GameSession()
         {
@@ -16,6 +17,15 @@ namespace Engine.ViewModels
                 ExperiencePoints = 0,
                 Level = 1,
                 Gold = 100
+            };
+
+            CurrentLocation = new Location
+            {
+                Name = "Casa",
+                XCoordinate = 0,
+                YCoordinate = -1,
+                Description = "Hogar tradicional porteño.",
+                ImageName = "F:\\Programacion\\Git\\TinyStudioRPG\\TinyStudioRPG\\Engine\\Images\\Locations\\casa.png"
             };
         }
     }
