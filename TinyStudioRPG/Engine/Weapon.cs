d75b0c41f1875d364fe7faf48a40d1ebcd3ef7cb
Mathias Falvo <falvomathias@gmail.com>
2025-04-22 11:41:21 -0300

    * Creo clase base para armas

---
+++ TinyStudioRPG/Engine/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using Engine.Models;
+
+namespace Engine
+{
+    public class Weapon : GameItem
+    {
+        public int MinimumDamage { get; set; }
+        public int MaximumDamage { get; set; }
+
+        public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage) : base(itemTypeID, name, price)
+        {
+            MinimumDamage = minDamage;
+            MaximumDamage = maxDamage;
+        }
+    }
+}
