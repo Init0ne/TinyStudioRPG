dbb3ff4ff1a4338d45e0d15edb79c6477371af58
Mathias Falvo <falvomathias@gmail.com>
2025-04-22 11:34:36 -0300

    * Agrego clase base para los items

---
+++ TinyStudioRPG/Engine/Models/GameItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Engine.Models
+{
+    public class GameItem
+    {
+        public int ItempTypeID { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public GameItem(int itemTypeID, string name, int price)
+        {
+            ItempTypeID = itemTypeID;
+            Name = name;
+            Price = price;
+        }
+    }
+}
