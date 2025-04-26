d0e68dd98a9bf372deeec8f3b0c2bb8920311f66
Mathias Falvo <falvomathias@gmail.com>
2025-04-26 13:26:43 -0300

    * Agrego clase monsters

---
+++ TinyStudioRPG/Engine/Models/Monster.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Engine.Models
+{
+    public class Monster : BaseNotificationClass
+    {
+        private int _hitPoints;
+
+        public string Name { get; private set; }
+        public string ImageName { get; set; }
+        public int MaximunHitPoints { get; private set; }
+        public int HitPoints
+        {
+            get { return _hitPoints; }
+            set
+            {
+                _hitPoints = value;
+                OnPropertyChanged(nameof(HitPoints));
+            }
+        }
+
+        public int RewardExperiencePoints { get; private set; }
+        public int RewardGold { get; private set; }
+
+        public ObservableCollection<ItemQuantity> Inventory { get; set; }
+
+        public Monster(string name, string imageName,
+            int maximumHitPoints, int hitPoints, int rewardExperiencePoints, int rewardGold)
+        {
+            Name = name;
+            ImageName = string.Format("/Engine;component/Images/Monsters/{0}", imageName);
+            MaximunHitPoints = maximumHitPoints;
+            HitPoints = hitPoints;
+            RewardExperiencePoints = rewardExperiencePoints;
+            RewardGold = rewardGold;
+            Inventory = [];
+        }
+    }
+}
