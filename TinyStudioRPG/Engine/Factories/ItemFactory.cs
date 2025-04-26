24c3773cc482d2d3fd9318b9a4565ca523372cf9
Mathias Falvo <falvomathias@gmail.com>
2025-04-26 13:42:31 -0300

    * Agrego generador de numeros random simple * Agrego items

--- TinyStudioRPG/Engine/Factories/ItemFactory.cs
+++ TinyStudioRPG/Engine/Factories/ItemFactory.cs
@@ -10,10 +10,14 @@ namespace Engine.Factories
         {
             _standardGameItems = [];
 
-            _standardGameItems.Add(new Weapon(1001, "Facon", 1, 1, 2));
+            _standardGameItems.Add(new Weapon(1001, "Facón", 1, 1, 2));
             _standardGameItems.Add(new Weapon(1002, "Hacha", 2, 2, 2));
-            _standardGameItems.Add(new GameItem(9001, "Diente de víbora", 2));
-            _standardGameItems.Add(new GameItem(9002, "Piel de víbora", 2));
+            _standardGameItems.Add(new GameItem(9001, "Diente de víbora", 1));
+            _standardGameItems.Add(new GameItem(9002, "Cuero de víbora", 2));
+            _standardGameItems.Add(new GameItem(9003, "Cola de rata", 1));
+            _standardGameItems.Add(new GameItem(9004, "Cuero de rata", 2));
+            _standardGameItems.Add(new GameItem(9005, "Colmillo de araña", 1));
+            _standardGameItems.Add(new GameItem(9006, "Tela de araña", 2));
         }
 
         public static GameItem CreateGameItem(int itemTypeId)
---
+++ TinyStudioRPG/Engine/RandomNumberGenerator.cs
@@ -0,0 +1,12 @@
+﻿namespace Engine
+{
+    public static class RandonNumberGenerator
+    {
+        private static readonly Random _generator = new();
+
+        public static int SimpleNumberBetween(int minValue, int maxValue)
+        {
+            return _generator.Next(minValue, maxValue + 1);
+        }
+    }
+}
