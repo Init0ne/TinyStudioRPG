beee7882f35b08b2ea7811f98a45e9f3a78e4272
Mathias Falvo <falvomathias@gmail.com>
2025-04-26 10:06:39 -0300

    * Creo constructor clase items * Creo clase quests

--- TinyStudioRPG/Engine/Models/ItemQuantity.cs
+++ TinyStudioRPG/Engine/Models/ItemQuantity.cs
@@ -2,6 +2,13 @@
 {
     public class ItemQuantity
     {
+        public int ItemID { get; set; }
+        public int Quantity { get; set; }
 
+        public ItemQuantity(int itemId, int quantity)
+        {
+            ItemID = itemId;
+            Quantity = quantity;
+        }
     }
 }
---
+++ TinyStudioRPG/Engine/Models/Quest.cs
@@ -0,0 +1,9 @@
+﻿namespace Engine.Models
+{
+    public class Quest
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
