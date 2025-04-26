fffb09050887c303cef9263adef696a8ca712486
Mathias Falvo <falvomathias@gmail.com>
2025-04-26 10:08:46 -0300

    * Modifico el constructor de la clase quest por el primary constructor

--- TinyStudioRPG/Engine/Models/Quest.cs
+++ TinyStudioRPG/Engine/Models/Quest.cs
@@ -1,26 +1,15 @@
 ﻿namespace Engine.Models
 {
-    public class Quest
+    public class Quest(int id, string name, string description, List<ItemQuantity> itemsToComplete, int rewardExperiencePoints, int rewardGold, List<ItemQuantity> rewardItems)
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public int ID { get; set; } = id;
+        public string Name { get; set; } = name;
+        public string Description { get; set; } = description;
 
-        public List<ItemQuantity> ItemsToComplete { get; set; }
+        public List<ItemQuantity> ItemsToComplete { get; set; } = itemsToComplete;
 
-        public int RewardExperiencePoints { get; set; }
-        public int RewardGold { get; set; }
-        public List<ItemQuantity> RewardItems { get; set; }
-
-        public Quest(int id, string name, string description, List<ItemQuantity> itemsToComplete, int rewardExperiencePoints, int rewardGold, List<ItemQuantity> rewardItems)
-        {
-            ID = id;
-            Name = name;
-            Description = description;
-            ItemsToComplete = itemsToComplete;
-            RewardExperiencePoints = rewardExperiencePoints;
-            RewardGold = rewardGold;
-            RewardItems = rewardItems;
-        }
+        public int RewardExperiencePoints { get; set; } = rewardExperiencePoints;
+        public int RewardGold { get; set; } = rewardGold;
+        public List<ItemQuantity> RewardItems { get; set; } = rewardItems;
     }
 }
