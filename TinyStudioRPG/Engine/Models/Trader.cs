14a2a6b15fb926fbfd78657a0c3fd45f8ec916dc
Mathias Falvo <falvomathias@gmail.com>
2025-04-27 18:41:51 -0300

    * Creo la clase del trader

---
+++ TinyStudioRPG/Engine/Models/Trader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+namespace Engine.Models
+{
+    public class Trader(string name) : BaseNotificationClass
+    {
+        public string Name { get; set; } = name;
+        public ObservableCollection<GameItem> Inventory { get; set; } = [];
+
+        public void AddItemToInventory(GameItem item)
+        {
+            Inventory.Add(item);
+        }
+        public void RemoveItemFromInventory(GameItem item)
+        {
+            Inventory.Remove(item);
+        }
+    }
+}
